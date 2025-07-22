e0806af16249719bc68b513e533372775b2f620c
Ilya Beliaev <info@php-dev.info>
2025-07-22 16:54:43 +0200

    init commit.

--- src/Infrastructure/Logging/LoggerFactory.php
+++ src/Infrastructure/Logging/LoggerFactory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace N3XT0R\XPub\Infrastructure\Logging;
 
-use Monolog\Handler\StreamHandler;
-use Monolog\Level;
 use Monolog\Logger;
 use N3XT0R\XPub\Infrastructure\Database\Database;
 use N3XT0R\XPub\Infrastructure\Logging\Handler\WPDBLogHandler;
@@ -17,12 +15,6 @@ class LoggerFactory
         $logger = new Logger($channel);
         $dbHandler = new WPDBLogHandler(Database::get());
         $logger->pushHandler($dbHandler);
-
-        if (defined('WP_DEBUG') && WP_DEBUG) {
-            $logFile = WP_CONTENT_DIR.'/uploads/xpub-logs/system.log';
-            $handler = new StreamHandler($logFile, Level::Notice);
-            $logger->pushHandler($handler);
-        }
 
         return $logger;
     }
