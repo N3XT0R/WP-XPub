f44bf9a0f65dc020d2f2e548b1a2eb2eb6624936
Ilya Beliaev <info@php-dev.info>
2025-07-28 01:32:58 +0200

    docs: added docs.

--- xpub.php
+++ xpub.php
@@ -10,7 +10,7 @@ Domain Path: /languages
 use N3XT0R\XPub\Adapter\WordpressPlugin;
 
 defined('ABSPATH') or die('No script kiddies please!');
-
 require_once __DIR__.'/vendor/autoload.php';
+include_once 'version.php';
 
 WordpressPlugin::init(__FILE__);
