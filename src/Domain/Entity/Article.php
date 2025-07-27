64d88affd44032c529ce7f5ef90bf168fc9bee7a
Ilya Beliaev <info@php-dev.info>
2025-07-27 23:17:56 +0200

    added attributes.

--- src/Domain/Entity/Article.php
+++ src/Domain/Entity/Article.php
@@ -9,11 +9,12 @@ final class Article
     public function __construct(
         public readonly int $postId,
         public readonly string $title,
+        public readonly string $content,
         public readonly ?string $excerpt = null,
         public readonly ?string $url = null
     ) {
-        if (empty($title)) {
-            throw new \InvalidArgumentException('Title is required.');
+        if (empty($title) || empty($content)) {
+            throw new \InvalidArgumentException('Title and content are required.');
         }
     }
 
@@ -22,6 +23,7 @@ final class Article
         return new self(
             postId: $post->ID,
             title: $post->post_title ?? '',
+            content: $post->post_content ?? '',
             excerpt: $post->post_excerpt ?: null,
             url: get_permalink($post)
         );
