9941ecbf8b28b2224e0f3f69f4844b97e296de37
Ilya Beliaev <info@php-dev.info>
2025-07-28 06:00:19 +0200

    readme.txt

--- xpub.php
+++ xpub.php
@@ -6,7 +6,7 @@ include_once 'version.php';
  * Plugin Name: xPub-Publisher (WP-XPub)
  * Description: Flexible Multi-Channel Auto Publisher for WordPress
  * Version: 1.0.0
- * Author: N3XT0R
+ * Author: Ilya Beliaev (N3XT0R)
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
