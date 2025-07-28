f42ec957ff721ce0877450ee3a7b9c8e8c6f6604
Ilya Beliaev <info@php-dev.info>
2025-07-28 03:21:22 +0200

    tests added

--- tests/Domain/Service/ArticlePublisherTest.php
+++ tests/Domain/Service/ArticlePublisherTest.php
@@ -2,10 +2,13 @@
 
 namespace N3XT0R\XPub\Tests\Domain\Service;
 
-use PHPUnit\Framework\TestCase;
-use N3XT0R\XPub\Domain\Service\ArticlePublisher;
+use Monolog\Handler\TestHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use N3XT0R\XPub\Domain\Contracts\PublisherInterface;
 use N3XT0R\XPub\Domain\Entity\Article;
+use N3XT0R\XPub\Domain\Service\ArticlePublisher;
+use PHPUnit\Framework\TestCase;
 
 class ArticlePublisherTest extends TestCase
 {
@@ -18,5 +21,20 @@ class ArticlePublisherTest extends TestCase
 
         $articlePublisher = new ArticlePublisher([$publisher]);
         $articlePublisher->publish(new Article(1, 'title', 'content'));
+    }
+
+    public function testPublishWillTriggerError(): void
+    {
+        $publisher = $this->createMock(PublisherInterface::class);
+        $publisher->expects($this->once())
+            ->method('publish')
+            ->willReturn(false);
+        $testHandler = new TestHandler();
+        $logger = new Logger('test', [$testHandler]);
+
+        $articlePublisher = new ArticlePublisher([$publisher], $logger);
+        $articlePublisher->publish(new Article(1, 'title', 'content'));
+        self::assertTrue($testHandler->hasErrorRecords());
+        self::assertTrue($testHandler->hasRecordThatContains('Publishing failed for post', Level::Error));
     }
 }
