666ff49e3b3323815f57af393205a58c6ca11ec7
Ilya Beliaev <info@php-dev.info>
2025-07-28 02:33:08 +0200

    tests fixed.

--- src/Infrastructure/Markdown/HtmlToMarkdownRenderer.php
+++ src/Infrastructure/Markdown/HtmlToMarkdownRenderer.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace N3XT0R\XPub\Infrastructure\Markdown;
 
+use League\HTMLToMarkdown\Converter\HeaderConverter;
 use League\HTMLToMarkdown\HtmlConverter;
 use N3XT0R\XPub\Domain\Contracts\HtmlToMarkdownRendererInterface;
 
 class HtmlToMarkdownRenderer implements HtmlToMarkdownRendererInterface
 {
     public function __construct(
-        private readonly HtmlConverter $converter = new HtmlConverter()
+        private readonly HtmlConverter $converter = new HtmlConverter(['header_style' => HeaderConverter::STYLE_ATX])
     ) {
     }
 
