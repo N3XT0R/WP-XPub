1584a9751827d9f316852a817fe1802ad8d5f42f
Ilya Beliaev <info@php-dev.info>
2025-07-28 19:18:48 +0200

    fix.

--- build/build.sh
+++ build/build.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 set -e
 
-PLUGIN_NAME="xpub-multi-channel-publisher"
-BUILD_DIR="build"
-DIST_DIR="dist"
-DEST="$BUILD_DIR/$PLUGIN_NAME"
+DIST_DIR=build/dist
+PLUGIN_NAME=xpub-multi-channel-publisher
+PLUGIN_DIR=$DIST_DIR/$PLUGIN_NAME
 
-# Clean up
-rm -rf "$DEST" "$DIST_DIR"
-mkdir -p "$DEST" "$DIST_DIR"
+rm -rf "$DIST_DIR"
+mkdir -p "$PLUGIN_DIR"
 
-# Copy plugin files
-rsync -a ./ "$DEST/" \
+rsync -av --delete \
   --exclude='.git' \
   --exclude='build' \
-  --exclude='dist' \
   --exclude='tests' \
   --exclude='.github' \
   --exclude='*.zip' \
   --exclude='phpunit.*' \
   --exclude='.gitignore' \
-  --exclude='.gitattributes'
-
-# Zip with folder included
-cd "$BUILD_DIR"
+  --exclude='.gitattributes' \
+  ./ "$PLUGIN_DIR/"
