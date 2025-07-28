ca1827e8cacefdc8fae4642a619e0a2090718b10
Ilya Beliaev <info@php-dev.info>
2025-07-28 11:31:24 +0200

    header changed.

--- xpub.php
+++ xpub.php
@@ -4,12 +4,19 @@ include_once 'version.php';
 
 /**
  * Plugin Name: WP-XPub
- * Description: Flexible Multi-Channel Auto Publisher for WordPress
+ * Plugin URI: https://github.com/N3XT0R/wp-xpub
+ * Description: Flexible multi-channel auto-publishing plugin using hexagonal architecture. Publish WordPress posts to external platforms like Mastodon, LinkedIn, Dev.to and more.
  * Version: 1.0.0
  * Author: Ilya Beliaev (N3XT0R)
+ * Author URI: https://github.com/N3XT0R
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: wp-xpub
  * Domain Path: /languages
+ * Requires at least: 6.0
+ * Tested up to: 6.8.2
+ * Requires PHP: 8.2
+ * Stable tag: 1.0.0
  */
 
 
