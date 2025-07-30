19a54050de8f6759bc5bd963937432b9a975cb40
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2025-07-30 21:02:22 +0200

    Add DI configuration

--- config/di.php
+++ config/di.php
@@ -1,7 +1,58 @@
 <?php
 
-use N3XT0R\XPub\Adapter\Outbound\Publishers\WordpressPublisher;
-use N3XT0R\XPub\Application\Service\PublishPost;
+declare(strict_types=1);
+
+use function DI\autowire;
+use function DI\create;
+use function DI\factory;
+use function DI\get;
+
+use N3XT0R\XPub\Application\Update\ReleaseService;
+use N3XT0R\XPub\Domain\Contracts\Factory\ArticleFactoryInterface;
+use N3XT0R\XPub\Domain\Contracts\Factory\WordpressArticleFactoryInterface;
+use N3XT0R\XPub\Domain\Contracts\HtmlToMarkdownRendererInterface;
+use N3XT0R\XPub\Domain\Contracts\ReleaseProviderInterface;
+use N3XT0R\XPub\Domain\Contracts\RendersPostContentInterface;
+use N3XT0R\XPub\Domain\Contracts\TranslatesMessagesInterface;
+use N3XT0R\XPub\Domain\Contracts\ViewInterface;
+use N3XT0R\XPub\Domain\Hook\FilterDispatcherInterface;
+use N3XT0R\XPub\Domain\Hook\HookDispatcherInterface;
+use N3XT0R\XPub\Domain\Repository\PublisherRepositoryInterface;
+use N3XT0R\XPub\Domain\Settings\SettingsRepositoryInterface;
+use N3XT0R\XPub\Domain\Contracts\QueueRepositoryInterface;
+use N3XT0R\XPub\Infrastructure\Markdown\HtmlToMarkdownRendererFactory;
+use N3XT0R\XPub\Infrastructure\Wordpress\Content\WpPostContentRenderer;
+use N3XT0R\XPub\Infrastructure\Wordpress\Database\Database;
+use N3XT0R\XPub\Infrastructure\Wordpress\Factory\ArticleFactory;
+use N3XT0R\XPub\Infrastructure\Wordpress\Hook\WordpressFilterDispatcher;
+use N3XT0R\XPub\Infrastructure\Wordpress\Hook\WordpressHookDispatcher;
+use N3XT0R\XPub\Infrastructure\Wordpress\Logging\LoggerFactory;
+use N3XT0R\XPub\Infrastructure\Wordpress\Repository\PublisherRepository;
+use N3XT0R\XPub\Infrastructure\Wordpress\Repository\WPDBQueueRepository;
+use N3XT0R\XPub\Infrastructure\Wordpress\Settings\WordpressSettingsRepository;
+use N3XT0R\XPub\Infrastructure\Wordpress\View\View;
+use N3XT0R\XPub\Infrastructure\Wordpress\I18n\Translator;
+use Psr\Log\LoggerInterface;
 
 return [
+    // Core repositories and services
+    SettingsRepositoryInterface::class => autowire(WordpressSettingsRepository::class),
+    PublisherRepositoryInterface::class => autowire(PublisherRepository::class),
+    QueueRepositoryInterface::class => factory(fn () => new WPDBQueueRepository(Database::get())),
+
+    // Factories and helpers
+    ArticleFactoryInterface::class => autowire(ArticleFactory::class),
+    WordpressArticleFactoryInterface::class => get(ArticleFactory::class),
+    RendersPostContentInterface::class => autowire(WpPostContentRenderer::class),
+    HtmlToMarkdownRendererInterface::class => factory([HtmlToMarkdownRendererFactory::class, 'create']),
+
+    // Views & translations
+    ViewInterface::class => create(View::class),
+    TranslatesMessagesInterface::class => create(Translator::class),
+
+    // Other utilities
+    ReleaseProviderInterface::class => autowire(ReleaseService::class),
+    FilterDispatcherInterface::class => create(WordpressFilterDispatcher::class),
+    HookDispatcherInterface::class => create(WordpressHookDispatcher::class),
+    LoggerInterface::class => factory([LoggerFactory::class, 'create']),
 ];
