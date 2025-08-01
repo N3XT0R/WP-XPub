80eb5f0afd53b8f1a12db3851fe5653c3a7b3e14
Ilya Beliaev <info@php-dev.info>
2025-08-01 22:21:31 +0200

    feature(mastodon): oauth authentification.

--- src/Infrastructure/Wordpress/Setup/Migrations/Migration_4.php
+++ src/Infrastructure/Wordpress/Setup/Migrations/Migration_4.php
@@ -14,7 +14,6 @@ class Migration_4 extends AbstractMigration
         OAuthPublisherSeederHelper::upsert('mastodon', 'Mastodon', [
             'grant_type' => 'authorization_code',
             'clientId' => '',
-            'clientSecret' => '',
             'urlAccessToken' => 'https://mastodon.social/oauth/token',
             'urlAuthorize' => 'https://mastodon.social/oauth/authorize',
             'urlResourceOwnerDetails' => 'https://mastodon.social/api/v1/accounts/verify_credentials',
