00afd6618aa23009aa3baa848e96c26037fb7372
Ilya Beliaev <info@php-dev.info>
2025-08-04 01:51:02 +0200

    tests fixed.

--- tests/Integration/Infrastructure/Wordpress/Hook/WordpressHookRegistrarTest.php
+++ tests/Integration/Infrastructure/Wordpress/Hook/WordpressHookRegistrarTest.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace N3XT0R\XPub\Tests\Infrastructure\Wordpress\Hook;
 
+use N3XT0R\XPub\Application\Update\ReleaseService;
+use N3XT0R\XPub\Infrastructure\DI\Cache\ContainerCacheCleaner;
 use N3XT0R\XPub\Infrastructure\OAuth\OAuthTokenProviderFactory;
 use N3XT0R\XPub\Infrastructure\Wordpress\Admin\SettingsSaveHandler;
 use N3XT0R\XPub\Infrastructure\Wordpress\Admin\Validator\SettingsFormRequestValidator;
@@ -13,6 +15,7 @@ use N3XT0R\XPub\Infrastructure\Wordpress\Repository\PublisherRepository;
 use N3XT0R\XPub\Infrastructure\Wordpress\Rest\OAuthController;
 use N3XT0R\XPub\Infrastructure\Wordpress\Settings\WordpressSettingsRepository;
 use N3XT0R\XPub\Infrastructure\Wordpress\Updater\PluginUpdateManager;
+use N3XT0R\XPub\Shared\Plugin\PluginContext;
 use N3XT0R\XPub\Tests\Stubs\DummyDispatcher;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\NullLogger;
@@ -31,14 +34,18 @@ final class WordpressHookRegistrarTest extends TestCase
                 new OAuthTokenProviderFactory(
                     new PublisherRepository(),
                     new WordpressSettingsRepository(),
+                    new NullLogger()
                 ),
                 new NullLogger()
             ),
             new PluginUpdateManager(
-                'plugin.php',
-                'xpub-multi-channel-publisher',
-                'https://example.com',
-                new \N3XT0R\XPub\Application\Update\ReleaseService()
+                $context = new PluginContext(
+                    'plugin.php',
+                    'xpub-multi-channel-publisher',
+                    'https://example.com',
+                ),
+                new ReleaseService(),
+                new ContainerCacheCleaner($context)
             )
         );
         $dispatcher = new DummyDispatcher();
