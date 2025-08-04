c3b282c8005e4fe4dd20dba851606fb5b64c8a93
Ilya Beliaev <info@php-dev.info>
2025-08-05 01:48:55 +0200

    json files.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-        react({
-            babel: {
-                plugins: [
-                    ['@wordpress/babel-plugin-makepot', {domain: 'xpub-multi-channel-publisher'}]
-                ]
-            }
-        }),
+        react(),
     ],
     define: {
         'process.env.NODE_ENV': JSON.stringify('production'),
